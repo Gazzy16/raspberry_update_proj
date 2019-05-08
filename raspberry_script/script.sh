ef327cd0f357fe7e319b1d0007be01bc0ad132f8
Gazzy16 <zidarovspas@gmail.com>
2019-05-08 18:06:35 +0100

    raspberry script

---
+++ raspberry_script/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd home/pi/raspberry_update_proj/program_files
+
+while [ 1 ]
+do
+
+pull=$(git pull)
+echo "$pull"
+if [ "$pull" == "Already up-to-date." ]; then
+	sshpass -p '2435' ssh gazzy@192.168.0.102 cd /home/gazzy/raspberry_update_proj/program_files && ./update.sh
+fi
+sleep 5
+done
