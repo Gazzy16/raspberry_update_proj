4d17bee76dcf06b795aafcb9a49991b5844a7fb8
Gazzy16 <zidarovspas@gmail.com>
2019-05-09 13:16:11 +0300

    m

--- program_files/main.c
+++ program_files/main.c
@@ -9,10 +9,13 @@ int main(int argc, char** argv){
 
 	int i = 0;
 	char l;
-	while(1){
+
+	printf("2v\n" );
+
+	while(i < 100){
 			i++;
-			printf("%d\n", i);
-			sleep(2);
+			//printf("%d\n", i);
+			sleep(1);
 	}
 
 	return 0;
--- program_files/update.sh
+++ program_files/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+cd /home/gazzy/raspberry_update_proj/program_files
 pkill -f ./proj
 git pull
 make
-./proj
+./proj &
